ccc40a3012c4a29d5a987605ed4c10792d5b25ef
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-03-20 15:49:15 +0530

    Update kas.sh

--- kas.sh
+++ kas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=us2.kaspa.herominers.com:1206
+POOL=us.acc-pool.pw:16061
 WALLET=kaspa:qzwzeazzje8fruw952gnnp5rfl5gwqtu60ldmmqw0huyp6674hzu50h7ufpu4
 
 ./lolMiner --algo KASPA --pool $POOL --user $WALLET $@
