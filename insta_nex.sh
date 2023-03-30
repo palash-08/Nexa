83d1bc0b9ef9a0a770d8c742a312f2a4c0aedfd9
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-03-30 22:03:36 +0530

    Update insta_nex.sh

--- insta_nex.sh
+++ insta_nex.sh
@@ -1,6 +1,7 @@
 cd
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.72/lolMiner_v1.72_Lin64.tar.gz
 tar -xvf lolMiner_v1.72_Lin64.tar.gz
+cd
 cd chialol
 mv nexa.sh 1.72
 cd 1.72 
