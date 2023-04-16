db417daee6711a27bb0e6c22a1836b2b6152b04a
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-04-16 09:51:17 +0530

    Update insta_nex.sh

--- insta_nex.sh
+++ insta_nex.sh
@@ -2,7 +2,7 @@ cd
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.73/lolMiner_v1.73_Lin64.tar.gz
 tar -xvf lolMiner_v1.73_Lin64.tar.gz
 cd
-cd chialol
+cd nexa
 mv nexa.sh /home/ubuntu/1.73
 cd
 cd 1.73
