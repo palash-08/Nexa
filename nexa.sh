9055085c1327c700f0173c393cf8cd2b839c5e92
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-04-23 09:41:17 +0530

    Update nexa.sh

--- nexa.sh
+++ nexa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=nexa.kryptex.network:7777
+POOL=na.nexa.pool137.io:3333
 WALLET=nexa:nqtsq5g5h0dwf90euq84jz6fa5nlq84ml09n64lcafu8kknf
 
 ./lolMiner --algo NEXA --pool $POOL --user $WALLET $@
