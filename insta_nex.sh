b4664f1bbff673a14bf0700d2c7b400a3cb69cfa
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-05-12 15:37:47 +0530

    Delete insta_nex.sh

--- insta_nex.sh
+++
@@ -1,9 +0,0 @@
-cd
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.73/lolMiner_v1.73_Lin64.tar.gz
-tar -xvf lolMiner_v1.73_Lin64.tar.gz
-cd
-cd nexa
-mv nexa.sh /home/ubuntu/1.73
-cd
-cd 1.73
-screen bash nexa.sh
