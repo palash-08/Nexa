8e22afae99b22d3c021aec7a8ac207966927a6db
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-05-12 15:36:28 +0530

    Create script-solo-nexa.sh

---
+++ script-solo-nexa.sh
@@ -0,0 +1,22 @@
+echo "Installing lolMiner..."
+
+cd
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.73/lolMiner_v1.73_Lin64.tar.gz
+tar -xvf lolMiner_v1.73_Lin64.tar.gz
+cd nexa
+mv solo-nexa.sh /home/ubuntu/1.73
+
+echo "Installing NVidia CUDA SDK..."
+cd
+cd 1.73
+sudo apt update
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+cd
+cd 1.73
+screen bash solo-nexa.sh
