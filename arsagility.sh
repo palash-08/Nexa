7010a49386ea02429847762c221bf877b1cbd5dd
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-06-03 22:46:37 +0530

    Update arsagility.sh

--- arsagility.sh
+++ arsagility.sh
@@ -3,4 +3,4 @@ cd
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.2.8/SRBMiner-Multi-2-2-8-Linux.tar.xz
 tar -xvf SRBMiner-Multi-2-2-8-Linux.tar.xz
 cd SRBMiner-Multi-2-2-8
-screen SRBMiner-MULTIi --algorithm ghostrider --pool stratum-na.rplant.xyz:17095 --tls true --wallet ATrFgpi8vn5TYWPoEgBAiRKnvntSkd4Pk5	--keepalive true
+screen SRBMiner-MULTI --algorithm ghostrider --pool stratum-na.rplant.xyz:17095 --tls true --wallet ATrFgpi8vn5TYWPoEgBAiRKnvntSkd4Pk5	--keepalive true
