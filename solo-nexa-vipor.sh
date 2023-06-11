84dadaa91db37e5778e01d27033de3a3cd2f61fc
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-06-11 11:20:59 +0530

    Update solo-nexa-vipor.sh

--- solo-nexa-vipor.sh
+++ solo-nexa-vipor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=us.nexasolo.k1pool.com:6394
+POOL=us.nexasolo.k1pool.com:6395
 WALLET=nexa:nqtsq5g5rkyrvafnrpvyxaxkc3tzye3h4murupyzllf7gnng
 
 ./lolMiner --algo NEXA --pool $POOL --user $WALLET $@
