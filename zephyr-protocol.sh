0ce78a2378e2e1dab5aa3699531d8cc44f5c709b
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-07-01 22:29:55 +0530

    Create zephyr-protocol.sh

---
+++ zephyr-protocol.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.3/xmrig-6.19.3-linux-x64.tar.gz
+tar -xvf xmrig-6.19.3-linux-x64.tar.gz
+cd xmrig-6.19.3
+clear
+sudo screen ./xmrig -k -a rx/0 -o ca-zephyr.miningocean.org:5462 -u ZEPHYR3Yfd5X67RCWARATbTakmA5BtoWtf1qEVkxWyPsJ7BxFFW2GyS1b2KikCKVW1EkoKkd1FAMyFuHuJXJZdz62AT6fQEPnDb1w
