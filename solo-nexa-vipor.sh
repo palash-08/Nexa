2555a9e41ac2cc4197a8b3233b847b2e129ef4d0
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-07-06 12:07:05 +0530

    Update solo-nexa-vipor.sh

--- solo-nexa-vipor.sh
+++ solo-nexa-vipor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo Hi
 echo Enter the worker number or name below...
 read workername
 screen ./lolMiner --algo NEXA --pool us.vipor.net:5085 --user nexa:nqtsq5g5h0dwf90euq84jz6fa5nlq84ml09n64lcafu8kknf.$workername --watchdog exit --apiport 8020
