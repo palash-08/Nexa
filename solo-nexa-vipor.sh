943b833614574f65b3640283c94c3842c67ca63d
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-07-06 11:50:17 +0530

    Update solo-nexa-vipor.sh

--- solo-nexa-vipor.sh
+++ solo-nexa-vipor.sh
@@ -2,4 +2,4 @@
 
 echo Enter the worker number or name below...
 read $workername
-./lolMiner -a NEXA --pool us.vipor.net:5085 -u nexa:nqtsq5g5h0dwf90euq84jz6fa5nlq84ml09n64lcafu8kknf.$workername
+screen ./lolMiner -a NEXA --pool us.vipor.net:5085 -u nexa:nqtsq5g5h0dwf90euq84jz6fa5nlq84ml09n64lcafu8kknf.$workername
