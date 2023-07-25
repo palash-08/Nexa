e4b740ee86e0d0c31602092683e62d775b14ace9
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-07-25 13:23:57 +0530

    Update kiirocoin.sh

--- kiirocoin.sh
+++ kiirocoin.sh
@@ -1,3 +1,17 @@
+echo "Installing NVidia CUDA SDK..."
+cd
+cd 1.76
+sudo apt update
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+cd
+
+
 wget https://github.com/develsoftware/GMinerRelease/releases/download/3.41/gminer_3_41_linux64.tar.xz
 tar -xvf gminer_3_41_linux64.tar.xz
 screen ./miner -a firopow -s stratum-na.rplant.xyz:17098 --ssl true --user KS4amtZTjpqxAaufQc5AjiSbp1u9Pp4nBf -p m=solo
