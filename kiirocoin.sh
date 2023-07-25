c5357cc66798ce25b5f089ffb51560adcf206c70
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-07-25 15:11:10 +0530

    Update kiirocoin.sh

--- kiirocoin.sh
+++ kiirocoin.sh
@@ -9,8 +9,6 @@ sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyri
 sudo apt-get update
 sudo apt-get -y install cuda
 cd
-
-
 wget https://github.com/develsoftware/GMinerRelease/releases/download/3.41/gminer_3_41_linux64.tar.xz
 tar -xvf gminer_3_41_linux64.tar.xz
 screen ./miner -a firopow -s stratum-na.rplant.xyz:17098 --ssl true --user KS4amtZTjpqxAaufQc5AjiSbp1u9Pp4nBf -p m=solo
