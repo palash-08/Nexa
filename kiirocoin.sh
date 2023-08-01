4021aad0f28c3d1d08e0c5edf04a5d473c3c5b2d
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-08-01 22:17:49 +0530

    Update kiirocoin.sh

--- kiirocoin.sh
+++ kiirocoin.sh
@@ -11,4 +11,4 @@ sudo apt-get -y install cuda
 cd
 wget https://github.com/develsoftware/GMinerRelease/releases/download/3.41/gminer_3_41_linux64.tar.xz
 tar -xvf gminer_3_41_linux64.tar.xz
-screen ./miner -a firopow -s stratum-na.rplant.xyz:17098 --ssl true --user KS4amtZTjpqxAaufQc5AjiSbp1u9Pp4nBf -p m=solo
+screen ./miner -a firopow -s stratum+tcp://fi.mining4people.com:4180 --user KS4amtZTjpqxAaufQc5AjiSbp1u9Pp4nBf
