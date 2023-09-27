0b302110f2bc71931a5be456b66ef7e554e21a28
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-09-27 19:18:59 +0530

    Update zephyr-protocol.sh

--- zephyr-protocol.sh
+++ zephyr-protocol.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.19.3/xmrig-6.19.3-linux
 tar -xvf xmrig-6.19.3-linux-x64.tar.gz
 cd xmrig-6.19.3
 clear
-sudo screen ./xmrig -k -a rx/0 -o zeph.us.ekapool.com:15667 -u ZEPHYR3Yfd5X67RCWARATbTakmA5BtoWtf1qEVkxWyPsJ7BxFFW2GyS1b2KikCKVW1EkoKkd1FAMyFuHuJXJZdz62AT6fQEPnDb1w
+sudo screen ./xmrig -k -a rx/0 -o us-zephyr.miningocean.org:5342 -u ZEPHYR3Yfd5X67RCWARATbTakmA5BtoWtf1qEVkxWyPsJ7BxFFW2GyS1b2KikCKVW1EkoKkd1FAMyFuHuJXJZdz62AT6fQEPnDb1w
