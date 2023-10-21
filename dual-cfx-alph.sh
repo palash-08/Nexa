dafb44d227951d52aa74339c7953e56b7ed1e513
Palash Patel <80760750+palash-08@users.noreply.github.com>
2023-10-21 15:39:11 +0530

    Update dual-cfx-alph.sh

--- dual-cfx-alph.sh
+++ dual-cfx-alph.sh
@@ -8,7 +8,7 @@
 # `--dual-mode a12:r64` - maximum impact on the primary algorithm
 
 # cfx + alph
-./rigel -a octopus+alephium \
+./rigel -a octopus+octopus+sha512256d \
     -o [1]stratum+tcp://de.conflux.herominers.com:1170         -u [1]cfx:aajn88av83p0671a2dj4c78ybf3e0yxt4pg4vj47ub	 \
-    -o [2]stratum+tcp://de.alephium.herominers.com:1199  -u [2]17P72JMyPTDjbg3bzwvyZhpab8dPD8kpw4UZprU28Y7ss \
+    -o [2]stratum+tcp://pool.vipor.net:5066  -u [2]19nebmX11bFuYw6Tt2cMHwH9qQCCj7LSAw \
     -w my_rig --log-file logs/miner.log
